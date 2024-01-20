fc66133b912f0af8c2d273d75749cb793133dd8c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-01-20 16:29:37 +0530

    All onclick event in react.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import Props from './Component/Props'
+import Event from './Component/Event';
 
 function App() {
-  const state = "Maharashtra";
-  let bike = {
-    company:"Bajaj",
-    model:"Pulser",
-    price:1.5
-  }
+  
   return (
     <div className="App">
-      <Props name = "Rushikesh" lname = "Tekale" state = {state} company = {bike}/>
+      <Event/>
     </div>
   );
 }
