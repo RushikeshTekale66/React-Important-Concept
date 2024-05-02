ae67989602a78ad9a91831d4fe1aa125017e0102
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-02 21:21:49 +0530

    useMemo() 2

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,38 +2,25 @@ import React from 'react'
 import { useState, useMemo } from 'react'
 
 const Home = () => {
-    const [myNum, setMyNum] = useState(0);
-    const [show, setShow] = useState(true);
-
-    const getValue=()=>{
-        return setMyNum(myNum+1);
-    }
-
-    const countNumber = (num)=>{
-        console.log("Count number", num);
-        for(let i=0; i<10000; i++){}
-            return num;
-    }
+    const [value1, setValue1] = useState(0);
+    const [value2, setValue2] = useState(0);
 
     // countNumber will run only when we chenge the myNum
     // This will increase the performance of the site
     // It always return the memorized value
-    const checkData = useMemo(()=>{
-        return countNumber(myNum);
-    }, [myNum]);
+    const sum = useMemo(()=>{
+        return value1+value2;
+    }, [value1, value2]);
 
     // const checkData = countNumber(myNum);
 
   return (
     <div>
-        <h1>useMemo</h1>
-        <button onClick={getValue} style={{backgroundColor:"red"}}>
-            Counter
-        </button>
-        <p>My new Number: {checkData}</p>
-        <button onClick={()=>setShow(!show)}>
-            {show?"You clicked me":"Click me plz"}
-        </button>
+        <p>value1 :  {value1}</p>
+        <p>value2 :  {value2}</p>
+        <p>Sum : {sum}</p>
+        <button onClick={()=>setValue1(value1+1)}>Increment value1</button>
+        <button onClick={()=>setValue2(value2+1)}>Increment value2</button>
     </div>
   )
 }
