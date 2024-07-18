d7e60e68314130adc28dbf52401cdf3efdd8f91c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-18 16:22:36 +0530

    Context api && useContext() hook

--- src/Context/Counter.js
+++ src/Context/Counter.js
@@ -6,6 +6,6 @@ export const ConterContext = createContext(null);
 export const ConterProvider = (props)=>{
     const [count, setCount] = useState(5);
 
-    // it return the two values the value is to return & function to update the value;
+    // it return the two values the value is to return & function to update the value
     return <ConterContext.Provider value={{count, setCount}}>{props.children}</ConterContext.Provider>
 }
