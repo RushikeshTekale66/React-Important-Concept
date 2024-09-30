b5c2f34846fb4fb855a882fee308634cd8652c09
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-30 19:36:50 +0530

    Use Effect

--- src/Component/State.js
+++ src/Component/State.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 const State = ()=>{
-    const [name, setName] = useState('Rushi');
-    const [color, setColor] = useState("Red");
+    const [count, setCount] = useState(0);
+    const [timer, setTimer] = useState(0);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setCount((count)=>count + 1);
+        }, 1000);
+    })
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setTimer((timer)=>timer+1);
+        },2000)
+    }, [])
 
     return (
         <>
-            <h1 >My Name is {name}</h1>
-            <button onClick={()=>setColor("Black")}>{color}</button>
+           <h1>Current Count is {count}</h1>
+           <h1>Timer is {timer}</h1>
         </>
     )
 }
